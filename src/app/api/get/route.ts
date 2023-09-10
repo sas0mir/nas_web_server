7c7029031d1a595a9b72746d139af51cc1a3751f
sas_mir <sas_mir@icloud.com>
2023-09-10 23:50:53 +0200

    some progress with streaming files from server

--- src/app/api/get/route.ts
+++ src/app/api/get/route.ts
@@ -1,20 +1,44 @@
-import { readFile } from "fs/promises";
+import { stat } from "fs/promises";
 import { NextRequest, NextResponse } from "next/server";
 import { join } from "path";
+import mime from "mime";
+import { createReadStream } from "fs";
+import { Http2ServerRequest, Http2ServerResponse } from "http2";
+import url from "url";
 
-export async function GET(request: NextRequest) {
+export async function GET(request: Http2ServerRequest, response: Http2ServerResponse) {
 
-    const filepath = request.nextUrl.searchParams.get('filepath') || '';
+    const reqQuery = url.parse(request.url, true).query;
+    const filepath = Array.isArray(reqQuery) ? reqQuery[0].filepath : reqQuery.filepath;
+    const mimeType = mime.getType(filepath) || 'image/jpeg';
+    const range = request.headers.range;
+    console.log('RANGE->', range);
+    if (!range) return NextResponse.json({success: false, error: 'No range'})
 
     const path = join(filepath)
-    
-    const file = await readFile(path);
-    //todo createReadStream(path).pipe(res) in res.writeHead(200), {Content-Length: todo length with fs.stat, Content-Type: video/mp4}
+    console.log('FILEPATH->', path);
+    try {
+        //todo createReadStream(path).pipe(res) in res.writeHead(200), {Content-Length: todo length with fs.stat, Content-Type: mime.getType(filepath)}
+        //const file = await readFile(path);
+        const size = (await stat(path)).size;
+        console.log('STATS->', size);
 
-    if (!file) {
-        //console.error(err);
-        return NextResponse.json({success: false, error: '!FILE TODO'})
-    } else {
-        return NextResponse.json({success: true, file: file})
+        const chunkSize = 10 ** 6; //1mb
+        const start = Number(range.replace(/\D/g, ''));
+        const end = Math.min(start + chunkSize, size - 1);
+        const contentLength = end - start + 1;
+        const headers = {
+            "Content-Range": `bytes ${start}-${end}/${size}`,
+            "Accept-Ranges": "bytes",
+            "Content-Length": contentLength,
+            "Content-Type": mimeType
+        }
+
+        response.writeHead(206, headers);
+        const mediaStream = createReadStream(path, {start, end});
+        mediaStream.pipe(response);
+    } catch(err) {
+        console.error('GET-FILE-ERROR->', err)
+        return NextResponse.json({success: false, error: err})
     }
 }
--- src/app/components/viewer.tsx
+++ src/app/components/viewer.tsx
@@ -8,13 +8,19 @@ export default function Viewer(props: any) {
 
     const {fileName, fileType} = getFileTypeAndName(file.name);
     //TODO
-    const getFile = async (path: string) => {
+    const getFile = (path: string) => {
         if (!path) console.log('!PATH ERROR');
+        console.log('PATH->', path);
         try {
-            await fetch(`/api/get?filepath=${path}`, {
+            fetch(`/api/get?filepath=${path}`, {
                 method: 'GET',
-            }).then(res => res.json()).then((folder) => {
-               // setFolder(folder?.files)
+            }).then(res => {
+                return res.body?.getReader().read()
+            }).then((stream) => {
+                //let decoder = new TextDecoder('utf-8)
+                //const {value, done} = stream;
+                //decoder.decode(value)
+                console.log('STREAM->', stream);
             })
             //setFolder(folder)
         } catch(err) {
@@ -23,20 +29,24 @@ export default function Viewer(props: any) {
     }
 
     const showImage = () => {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            console.log('TARGET->', e.target);
-            const source = e.target?.result || '';
-            //return <img src={source} className={styles.viewer_image} />
-        }
-        reader.readAsBinaryString(file);
-        console.log('WIEW->', file);
-        return <div></div>
+        //const fileStream = await getFile(file.path + '/' + file.name)
+        // const reader = new FileReader();
+        // reader.onload = (e) => {
+        //     console.log('TARGET->', e.target);
+        //     const source = e.target?.result || '';
+        //     //return <img src={source} className={styles.viewer_image} />
+        // }
+        // reader.readAsBinaryString(file);
+        // console.log('WIEW->', file);
+        return <div>image</div>
     }
 
   return (
     <div className={styles.viewer_container}>
-        {/* {showImage()} */}
+        <video controls muted autoPlay>
+            <source src={`/api/get?filepath=${file.path}/${file.name}`} type="video/mp4" />
+        </video>
+        {showImage()}
         <p>{file.name}</p>
     </div>
   )
